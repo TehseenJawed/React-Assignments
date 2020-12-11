b3983de7ef37e0c0ad6674b06a7568f5300b2523
Tehseen Jawed <tehseenjawed1@gmail.com>
2020-12-11 10:00:23 +0500

    Updated the code

---
+++ darktolight/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="darktoLight">
+     <div className="dark">
+       <div className="btnAnimator">
+       <div className="button">
+
+       </div>
+       </div>
+     </div>
+    </div>
+  );
+}
+
+export default App;
